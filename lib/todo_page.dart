2c2ab904ceb32e0734d08e3cc2620755f3701875
hi-lufz1 <latifufauzi@gmail.com>
2025-03-18 15:45:31 +0700

    menambahkan text field dan button

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -10,6 +10,7 @@ class TodoPage extends StatefulWidget {
 class _TodoPageState extends State<TodoPage> {
 
    String _selectedDate = "Select a date";
+   final TextEditingController _nameController = TextEditingController();
 
 
 
@@ -30,7 +31,31 @@ class _TodoPageState extends State<TodoPage> {
                   , Text(_selectedDate)],),
                   IconButton(onPressed: (){}, icon: Icon(Icons.calendar_today, color: Colors.blue,) )
                 ],
-              )
+              ),
+              Form(
+                child: Row( children: [
+              Expanded(
+                  child: TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
+                    label: Text("First Name"),
+                    hintText: "Enter your first name"
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "task tidak boleh kosong";
+                    }
+                    return null;
+                  } ,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                ),
+                ),
+                FilledButton(onPressed: (){
+                }, child: Text("Submit"))
+                ]
+                ,)
+              ),
             ]
           ),)),
     );
