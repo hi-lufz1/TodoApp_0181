2a8ba81b99a96d82e2b4bd8928de905fa242d44a
hi-lufz1 <latifufauzi@gmail.com>
2025-03-18 15:18:31 +0700

    menambahkan text dan icon untuk date

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -15,6 +15,19 @@ class _TodoPageState extends State<TodoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Form Page", style: TextStyle(fontSize: 24),), centerTitle: true,),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column( children: [Text("Task date")],),
+                  IconButton(onPressed: (){}, icon: Icon(Icons.calendar_today, color: Colors.blue,) )
+                ],
+              )
+            ]
+          ),)),
     );
   }
 }
